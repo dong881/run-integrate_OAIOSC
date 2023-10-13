afc916be44a8d4788af0ce5cf8f9728f8d8aa1f2
dong881 <joe888joejoe@gmail.com>
2023-10-13 16:12:25 +0800

    edit nic scrip

--- nic.sh
+++ nic.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-sudo ifconfig eno8303:ODU  "192.168.130.81"
-sudo ifconfig eno8303:CU_STUB "192.168.130.82"
-sudo ifconfig eno8303:RIC_STUB "192.168.130.80"
-sudo ifconfig eno8303:OAI_CU "192.168.130.83"
+echo "***** Assigning IP addresses *****"
+INTERFACE=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
+INTERFACE="$(echo -e "${INTERFACE}" | tr -d '[:space:]')"
+
+sudo ifconfig $INTERFACE:ODU  "192.168.130.81"
+sudo ifconfig $INTERFACE:CU_STUB "192.168.130.82"
+sudo ifconfig $INTERFACE:RIC_STUB "192.168.130.80"
+sudo ifconfig $INTERFACE:OAI_CU "192.168.130.83"
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/home/gnb/
 export PATH
